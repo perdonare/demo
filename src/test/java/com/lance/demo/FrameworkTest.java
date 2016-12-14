994590bbb59cafc85e9144993fad92d9da0259ff
袁其亮 <yuanqiliang@51kik.com>
2016-12-14 20:46:42 +0800

    init

---
+++ src/test/java/com/lance/demo/FrameworkTest.java
@@ -0,0 +1,13 @@
+package com.lance.demo;
+
+import org.junit.Test;
+
+/**
+ * Created by perdonare on 2016/12/14.
+ */
+public class FrameworkTest {
+    @Test
+    public void testFrameWork() {
+        System.out.println("hello world");
+    }
+}
